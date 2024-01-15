9074117d2306bb148ae7337e1cfd3d4a640becca
Arsh Kochhar <arshkochhar@arshs-mbp.home>
2024-01-14 23:34:02 -0500

    initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,144 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import WalletIcon from "@mui/icons-material/Wallet";
+
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const gradient1 =
+    "linear-gradient(90deg, rgba(67,79,101,1) 0%, rgba(73,116,255,1) 84%, rgba(0,212,255,1) 100%)";
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar
+      sx={{
+        background: "#14233D",
+      }}
+      position="static"
+    >
+      <Container sx={{ padding: "10px" }} maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            ></Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Button
+              sx={{
+                borderRadius: "50px",
+                padding: "10px",
+                background: gradient1,
+                width: "200px",
+              }}
+              variant="contained"
+            >
+              <WalletIcon sx={{ marginRight: "5px" }}></WalletIcon> Connect
+              Wallet
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default NavBar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,148 @@
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import Paper from "@mui/material/Paper";
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import ManageHistoryIcon from "@mui/icons-material/ManageHistory";
+import NavBar from "../components/NavBar";
+import { Typography } from "@mui/material";
+
+const gradient1 =
+  "linear-gradient(90deg, rgba(67,79,101,1) 0%, rgba(73,116,255,1) 84%, rgba(0,212,255,1) 100%)";
+
+const CustomTab = styled(Tab)({
+  minWidth: 300, // adjust as needed
+
+  width: "", // adjust as needed
+  marginRight: 30, // adds space between tabs
+  color: "white",
+
+  "&:hover": {
+    background:
+      "linear-gradient(90deg, rgba(67,79,101,1) 0%, rgba(73,116,255,1) 84%, rgba(0,212,255,1) 100%)", // the color when hovering over a tab
+    borderRadius: 50,
+  },
+  "&.Mui-selected": {
+    background:
+      "linear-gradient(90deg, rgba(67,79,101,1) 0%, rgba(73,116,255,1) 84%, rgba(0,212,255,1) 100%)", // the color when a tab is selected
+    color: "white",
+    borderRadius: 50,
+  },
+  "&.Mui-focusVisible": {
+    background:
+      "linear-gradient(90deg, rgba(67,79,101,1) 0%, rgba(73,116,255,1) 84%, rgba(0,212,255,1) 100%)", // the color when a tab is focused
+    borderRadius: 50,
+  },
+});
+
+const CustomTabs = styled(Tabs)({
+  "& .MuiTabs-flexContainer": {
+    backgroundColor: "#14233D",
+    borderRadius: 50,
+    padding: "20px",
+  },
+  "& .MuiTabs-indicator": {
+    backgroundColor: "transparent",
+  },
+});
+
+export default function HomePage() {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          background:
+            "radial-gradient(circle, #051937, #182342, #272e4d, #363958, #454464);",
+          pt: "13vh", // 1/3rd from the top
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <CustomTabs value={value} onChange={handleChange} variant="fullWidth">
+            <CustomTab
+              iconPosition="start"
+              icon={<AddBoxIcon></AddBoxIcon>}
+              label="Create"
+            />
+            ManageHistoryIcon
+            <CustomTab
+              iconPosition="start"
+              icon={<ManageHistoryIcon></ManageHistoryIcon>}
+              label="Manage"
+            />
+          </CustomTabs>
+          <Paper
+            elevation={4}
+            sx={{
+              width: "100%",
+              maxWidth: 500,
+              mt: 2, // space between tabs and content
+              mb: 2, // space at the bottom
+              p: 3,
+              borderRadius: "16px",
+              bgcolor: "#14233D", // The solid background color
+              color: "white",
+              position: "relative",
+              overflow: "hidden", // Ensures the pseudo-element doesn't overflow
+              "&::after": {
+                // Create the gradient overlay
+                content: '""',
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                backgroundImage:
+                  "linear-gradient(145deg, rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0))",
+                borderRadius: "16px", // Ensure the overlay has the same border radius
+              },
+            }}
+          >
+            {value === 0 && (
+              <Box>
+                <Box sx={{ position: "relative", zIndex: 1 }}>
+                  {/* Icon and the rest of your content */}
+                  <Box sx={{ display: "flex", alignItems: "center", mb: 2 }}>
+                    {/* <CreditCardIcon sx={{ fontSize: 'large', mr: 1 }} /> */}
+                    <Typography variant="subtitle1" gutterBottom>
+                      VISA
+                    </Typography>
+                  </Box>
+                  <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                    Active Balance
+                  </Typography>
+                  <Typography variant="h4" sx={{ my: 1 }}>
+                    € 45.720
+                  </Typography>
+                  <Typography variant="body2">F Alexandra</Typography>
+                  <Typography variant="body2">05/24</Typography>
+                </Box>
+              </Box>
+            )}
+            {value === 1 && <Box>Content for Manage</Box>}
+          </Paper>
+        </Box>
+      </Box>
+    </>
+  );
+}
